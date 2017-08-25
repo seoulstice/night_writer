1a79d667f4f631e8939c9ec49490acb47693f652
seoulstice <jungyoungjoon@gmail.com>
2017-08-25 17:33:32 -0600

    modified read/write methods

--- lib/night_write.rb
+++ lib/night_write.rb
@@ -1,20 +1,32 @@
 
 class NightWriter
   attr_reader :raw_input,
-              :file_contents
+              :output_line_1,
+              :output_line_2,
+              :output_line_3
   def initialize
-    @file_contents
-    @raw_input = ""
+    @raw_input = raw_input
+    @output_line_1 = ""
+    @output_line_2 = ""
+    @output_line_3 = ""
     # @file = ARGV[0]
   end
 
-  # def get_input_filename
+  # def get_input_file
   #   filename = ARGV[0]
   #   filename
   # end
 
-  def open_file(file)
-    @file_contents = File.open(file)
+  def open_file
+    input_file = File.open(ARGV[0], "r")
+    @raw_input = input_file.read
+    input_file.close
+  end
+
+  def write_file
+    output_file = File.open(ARGV[1], "w")
+    output_file.write(contents to be written to file)
+    output_file.close
   end
 
   def parse_file_contents_to_string
@@ -27,9 +39,15 @@ class NightWriter
     end
   end
 
-  
+  def convert_capitalized_letters
+
+  end
+
+  def convert_raw_input_to_braille
+    # @raw_input.each do |letter|
 
 
+  end
 
   def count_output_characters
 
--- test/night_write_test.rb
+++ test/night_write_test.rb
@@ -20,11 +20,11 @@ binding.pry
 
   def test_it_can_open_file
     nw = NightWriter.new
-    nw.open_file("./data/message.txt")
+    nw.open_file
     nw.parse_contents_to_string
 
     assert_equal "Hello, World!", nw.raw_input
-  end 
+  end
 
   def test_it_can_count_characters_of_output
     skip
