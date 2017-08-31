b502679676e68863c00bf4aa3381e073b45c77cf
seoulstice <jungyoungjoon@gmail.com>
2017-08-30 22:25:01 -0600

    clean up files

---
+++ test/night_writer_test.rb
@@ -0,0 +1,33 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/night_write'
+require 'pry'
+
+class NightWriterTest < Minitest::Test
+
+  def test_night_writer_exists
+    nw = NightWriter.new
+
+    assert_instance_of NightWriter, nw
+  end
+
+  def test_night_writer_has_empty_string
+    nw = NightWriter.new
+
+    assert_instance_of String, nw.raw_message
+    assert_equal "", nw.raw_message
+  end
+
+  def test_it_can_open_file
+    nw = NightWriter.new
+    nw.open_file
+    nw.parse_contents_to_string
+
+    assert_equal "Hello, World!", nw.raw_message
+  end
+
+  def test_it_can_count_characters_of_output
+    skip
+  end
+
+end
